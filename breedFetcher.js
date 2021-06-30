543a082446d717f80d5ff48efb7e5287d62d437f
Afsanul Haque Khan <afsanhk@gmail.com>
2021-06-30 22:38:36 +0000

    Updated breedFetcher.js to also recognize 404 statusCode for invalid URL

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,23 @@
 const request = require('request');
 
-const breedName = process.argv.slice(2);
+const breedName = process.argv[2];
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
   
-  if (response === undefined) {
+  if (response === undefined || response.statusCode === 404) {
     console.log(`Invalid URL`);
+    process.exit();
   }
 
   if (error) {
     return `Error: ${error}`;
   }
-
+  
   const data = JSON.parse(body);
+  
   if (data.length === 0) {
     console.log(`Please enter an appropriate breed name.`);
+    process.exit();
   } else {
     console.log(data[0].description);
   }
